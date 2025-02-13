f718e6ccb3612c58a5d860c59285ab47c39ef9c3
TidusWR <harllemnascimento@gmail.com>
2025-02-12 21:12:20 -0300

    reorganiza pastas do projeto

---
+++ backend/movies-server/src/main/java/com/tiduswr/movies_server/MoviesServerApplication.java
@@ -0,0 +1,13 @@
+package com.tiduswr.movies_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesServerApplication.class, args);
+	}
+
+}
---
+++ backend/movies-server/src/test/java/com/tiduswr/movies_server/MoviesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiduswr.movies_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
