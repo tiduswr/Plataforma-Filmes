711a89afefd0c38ffe421f4c8d861b2eb27c747c
TidusWR <harllemnascimento@gmail.com>
2025-02-15 15:49:03 -0300

    configura chave publica e privada nos beans jwt

--- backend/movies-server/src/main/java/com/tiduswr/movies_server/config/SecurityConfig.java
+++ backend/movies-server/src/main/java/com/tiduswr/movies_server/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.tiduswr.movies_server.config;
 
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,12 +10,26 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     
+    @Value("${jwt.public.key}")
+    private RSAPublicKey pubKey;
+
+    @Value("${jwt.private.key}")
+    private RSAPrivateKey privKey;
+
     @Value("${app.security.csrf:true}")
     private boolean csrfEnabled;
 
@@ -31,5 +48,26 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    @Bean  
+    public JwtEncoder jwtEncoder() {  
+        // Cria uma chave JWK (JSON Web Key) a partir da chave pública e privada RSA  
+        var jwk = new RSAKey.Builder(pubKey)  
+                        .privateKey(privKey)  // Define a chave privada  
+                        .build();             // Constrói o objeto JWK  
+
+        // Cria um conjunto imutável de JWK contendo a chave criada  
+        var jwks = new ImmutableJWKSet<>(new JWKSet(jwk));  
+
+        // Retorna um codificador JWT utilizando o conjunto de chaves  
+        return new NimbusJwtEncoder(jwks);  
+    }  
+
+    @Bean  
+    public JwtDecoder jwtDecoder() {  
+        // Retorna um decodificador JWT utilizando a chave pública RSA  
+        return NimbusJwtDecoder.withPublicKey(pubKey).build();  
+    }  
+
 
 }
--- backend/movies-server/src/main/resources/data.sql
+++ backend/movies-server/src/main/resources/data.sql
@@ -1,1 +1,2 @@
+-- Apenas para testes
 INSERT INTO tb_roles (role_id, name) VALUES (1, 'admin'), (2, 'user')
