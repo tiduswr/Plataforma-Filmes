525ab7ec2a007fe8eff7d985bb9f4172d0ad53d9
TidusWR <harllemnascimento@gmail.com>
2025-02-21 01:05:27 -0300

    adiciona imports na classe movies-server/**/LoginReques.java

--- backend/movies-server/src/main/java/com/tiduswr/movies_server/models/dto/LoginRequest.java
+++ backend/movies-server/src/main/java/com/tiduswr/movies_server/models/dto/LoginRequest.java
@@ -1,5 +1,8 @@
 package com.tiduswr.movies_server.models.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record LoginRequest(
     @NotNull(message = "O campo username é obrigatório")
     @Size(min = 5, max = 30, message = "O campo username precisa ter no mínimo 5 e no máximo 30 caracteres")
